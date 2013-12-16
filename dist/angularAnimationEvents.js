b3b1d70e763f1533ee19181c66479fb18cb2ae0e
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2013-12-16 16:08:37 +0100

    Copied the code from the original repo

---
+++ dist/angularAnimationEvents.js
@@ -0,0 +1,63 @@
+(function() {
+
+  var module = angular.module('angularAnimationEvents', []);
+
+  module.constant("CAEEventList", [
+    "webkit{{type}}End",
+    "{{type | lowercase}}End",
+    "o{{type}}End",
+    "MS{{type}}End"
+  ]);
+
+  module.service("CAEListenerService", function($interpolate, CAEEventList) {
+    this.observe = function(iElement, scope, eventFunc, eventType) {
+      var subs = CAEEventList.map(function(a){
+        var func = $interpolate(a);
+        return func({type: eventType});
+      });
+
+      for(var i=0;i<subs.length;i++) {
+        iElement[0].addEventListener(subs[i], function(evt) {
+          scope[eventFunc](eventType.toLowerCase().concat('End'), evt);
+        })
+      }
+    };
+  });
+
+  module.directive('emitAnimationEnd', function(CAEListenerService){
+    return {
+      restrict: 'A',
+      link: function(scope, iElement, attrs) {
+        CAEListenerService.observe(iElement, scope, '$emit', 'Animation');
+      }
+    }
+  });
+
+  module.directive('emitTransitionEnd', function(CAEListenerService){
+    return {
+      restrict: 'A',
+      link: function(scope, iElement, attrs) {
+        CAEListenerService.observe(iElement, scope, '$emit', 'Transition');
+      }
+    }
+  });
+
+  module.directive('broadcastAnimationEnd', function(CAEListenerService){
+    return {
+      restrict: 'A',
+      link: function(scope, iElement, attrs) {
+        CAEListenerService.observe(iElement, scope, '$broadcast', 'Animation');
+      }
+    }
+  });
+
+  module.directive('broadcastTransitionEnd', function(CAEListenerService){
+    return {
+      restrict: 'A',
+      link: function(scope, iElement, attrs) {
+        CAEListenerService.observe(iElement, scope, '$broadcast', 'Transition');
+      }
+    }
+  });
+
+})();
