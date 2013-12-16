0ebb00fa4cfaffa8ad485d0e090f71a5aa1041b2
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2013-12-16 16:09:13 +0100

    Preparing for unit testing

---
+++ test/animationEventsSpec.js
@@ -0,0 +1,9 @@
+describe("Unit: angularAnimationEvents module", function() {
+
+  it("should be registered", function() {
+    expect(angular.module('angularAnimationEvents')).toNotEqual(null);
+  });
+
+});
+
+});
