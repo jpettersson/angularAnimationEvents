b87aa417e9f698ada26bbe3db7b451272285980d
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2013-12-18 16:50:57 +0100

    Added a setup rask for installing webDriver

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,18 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-protractor-runner');
 
   grunt.initConfig({
+    shell: {
+      installWebDriver: {
+        command: "./node_modules/protractor/bin/webdriver-manager update",
+        options: {
+          stdout: true
+        }
+      }
+    },
     karma: {
       watch: {
         configFile: 'test/karma.config.js',
@@ -29,5 +38,6 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('setup', ['shell:installWebDriver']);
   grunt.registerTask('test', ['karma:singleRun', 'connect', 'protractor:e2e']);
 };
